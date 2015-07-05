bc05bb7048785be888ceb8b2d2aa1eec48f967bf
soilcheng <moyancheng@gmail.com>
2015-07-05 21:39:59 +0800

    update

---
+++ models/emailModel.php
@@ -0,0 +1,180 @@
+<?php
+/*
+作者:moyancheng
+创建时间：2013-11-01
+最后更新时间:2013-11-12
+*/
+/*
+$time = time();
+$host = 'smtp.qq.com';
+$pors = 25;
+$user = 'email@mo2g.com';
+$pass = '***';
+
+$smtp = new emailModel($host,$pors,$user,$pass); 
+
+$to = 'moyancheng@gmail.com';
+$from = 'email@mo2g.com';
+$subject = '邮件测试'.$time;
+$body = '测试成功';
+$from_email = 'email@mo2g.com';
+$from_name = '磨途歌';
+	
+if($smtp->send($to,$from,$subject,$body,$from_email,$from_name) ) {
+	echo $time;
+}
+*/
+/*
+ $smtp->smtp('smtp.qq.com');           //邮件发送服务器 
+ $smtp->pors(25);                      //邮件服务器端口 
+ $smtp->login( '123@qq.com');     //邮件服务器登录用户 
+ $smtp->pass('***');             //邮件服务器登录密码 
+ $smtp->mails( 'moyancheng@gmail.com');    //邮件接收人 
+ $smtp->from( '123@qq.com');      //邮件发送人 
+ $smtp->come( '磨途歌<email@mo2g.com>');  //接收邮件回复地址 
+ $smtp->title('这是一封邮件测试地址邮件'.time());  //邮件主题 
+ $smtp->body('这是一封邮件测试内容邮件');   //邮件内容 
+ echo $smtp->send();                   //发送邮件并输出发送状态 
+*/
+ 
+ 
+/*
+SMTP发送邮件类
+fsockopen()
+set_socket_blocking();0是非阻塞，1是阻塞
+fgets
+fputs
+*/
+class emailModel {
+	private $fp;
+	private $host;
+	private $post;
+	private $user;
+	private $pass;
+	
+	/*
+	$host	服务器地址
+	$post	端口
+	$user	用户名
+	$pass	密码
+	*/
+	public function __construct($host = '',$post = '',$user = '',$pass = '') {
+		$this->fp = false;
+		$this->host = $host;
+		$this->post = $post;
+		$this->user = $user;
+		$this->pass = $pass;
+	}
+	
+	//跟邮箱服务器通信
+	public function connection($host = '',$post = '',$user = '',$pass = '') {
+		if( $this->fp == false) {
+			$host = $host ? $host : $this->host;
+			$post = $post ? $post : $this->post;
+			$user = $user ? $user : $this->user;
+			$pass = $pass ? $pass : $this->pass;
+			
+			$fp = fsockopen( $host, $post);
+			// @set_socket_blocking($fp, true);
+			
+			//向邮件服务器发送连接请求
+			$Server = fgets($fp, 512);
+			$this->check($Server,'220');
+			
+			//向服务器发送会话请求
+			fputs($fp, "HELO phpsetmail\r\n");
+			$Server = fgets($fp, 512);
+			$this->check($Server,'250');
+			
+			//请求验证用户名，密码
+			fputs($fp, "AUTH LOGIN\r\n");
+			$Server = fgets($fp, 512);
+			$this->check($Server,'334');
+			
+			//发送用户名
+			fputs($fp, base64_encode($user) . "\r\n");
+			$Server = fgets($fp, 512);
+			$this->check($Server,'334');
+			
+			//发送密码
+			fputs($fp, base64_encode($pass) . "\r\n");
+			$Server = fgets($fp, 512);
+			$this->check($Server,'235');
+			
+			$this->fp = $fp;
+		}
+		return $this;
+	}
+	
+	//关闭连接
+	public function close() {
+		if( $this->fp != false ) {
+			fputs($this->fp, "QUIT\r\n");
+			fclose($this->fp);
+			$this->fp = false;
+		}
+	}
+	
+	/*
+	$to	接收地址
+	$from	发送地址
+	$subject	主题
+	$body	内容
+	$from_email	自定义发送地址
+	$from_name	自定义发送名称
+	*/
+	public function send($to,$from,$subject,$body,$from_email = '',$from_name = '') {
+		$this->connection();
+		
+		//指定邮件发送邮箱
+		fputs($this->fp, "MAIL FROM:{$from}\r\n");
+		$Server = fgets($this->fp, 512);
+		$this->check($Server,'250');
+	
+		//指定邮件接收邮箱
+		fputs($this->fp, "RCPT TO:{$to}\r\n");
+		$Server = fgets($this->fp, 512);
+		$this->check($Server,'250');
+		
+		//发送邮件数据
+		fputs($this->fp, "DATA\r\n");
+		$Server = fgets($this->fp, 512);
+		$this->check($Server,'354');
+		
+		$header = "MIME-Version:1.0\r\n";
+		$header .= "Content-Type:text/html;charset=\"utf-8\"\r\n";
+		$header .= "Content-Transfer-Encoding: base64\r\n";
+		if( $from_email == '' ) {
+			$header .= 'From: =?utf-8?b?'.base64_encode($from)."?=\r\n";//邮件回复地址
+		} else {
+			$header .= 'From: =?utf-8?b?'.base64_encode($from_name)."?=<{$from_email}>\r\n";//邮件回复地址
+		}
+		$subject = '=?utf-8?b?'.base64_encode($subject).'?=';//编码邮件标题，解决乱码问题
+		
+		$body = base64_encode($body);//编码邮件标题，解决乱码问题
+		$header .= "Subject:{$subject}\r\n";//邮件主题
+		$header .= "To:{$to}\r\n";//邮件接收人
+		$strEmail = "{$header}\r\n{$body}\r\n.\r\n";
+		
+		fputs($this->fp, $strEmail);
+		$Server = fgets($this->fp, 512);
+		
+		$this->check($Server,'250');
+		
+		//fputs($this->fp, "QUIT\r\n");
+		//fclose($this->fp);
+		return true;
+	}
+	
+	//检测服务器返回值
+	public function check($strInfo,$strKey) {
+		if( substr($strInfo,0,3) == $strKey ) {
+			return true;
+		} else {
+			echo $strInfo;
+			fclose($this->fp);
+			exit;
+		}
+	}
+	
+}
---
+++ models/memcachedModel.php
@@ -0,0 +1,87 @@
+<?php
+/*
+作者:moyancheng
+创建时间:2013-05-13
+最后更新时间:2015-07-05
+功能：
+1.封装memcached常用操作
+2.添加like匹配操作
+*/
+class memcachedModel {
+	private $mem;
+	public function __construct($arrConfig = array() ) {
+		$mark = isset( $arrConfig['mark'] ) ? $arrConfig['mark'] : $_SERVER['SERVER_NAME'];//标记mem服务
+
+		$mem = new Memcached($mark);
+		if( isset($arrConfig['auth']) && isset($arrConfig['password']) ) {
+			//需要认证的mem服务
+			if( count( $mem->getServerList() ) == 0 ) {
+				$mem->setOption(Memcached::OPT_COMPRESSION, false);
+				$mem->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+				$mem->addServer($arrConfig['host'],$arrConfig['port']);
+				$mem->setSaslAuthData($arrConfig['auth'], $arrConfig['password']);
+			}
+		} else {
+			$mem->addServer($arrConfig['host'],$arrConfig['port']);//不需要认证的mem服务
+		}
+		$this->mem = $mem;
+	}
+
+	/**
+	 * 写缓存
+	 * @param $key 缓存名称
+	 * @param $data 缓存内容
+	 * @param $expire 缓存有效期，0,长期有效。
+	 * @return integer
+	 */
+	public function set($key,$data,$expire=0) {
+		return $this->mem->set($key,$data,$expire);
+	}
+
+	/**
+	 * 取缓存
+	 * @param string $key 缓存名称
+	 * @return mixed
+	 */
+	public function get($key) {
+		return $this->mem->get($key);
+	}
+	
+	public function get_all() {
+		return $this->mem->fetchAll();
+	}
+	
+	/*
+	模拟like匹配操作
+	$like:为正则表达式
+	*/
+	public function like($like,$key = '') {
+		$arrData = $arrTmp = array();
+		if($key !== '')$arrTmp = $this->get($key);
+		else  $arrTmp = $this->get_all();
+		
+		foreach($arrTmp as $key => $data) {
+			if( preg_match($like,$key) ) $arrData[$key] = $data;
+		}
+		unset($arrTmp);
+		return $arrData;
+	}
+
+	/**
+	 * 删除缓存
+	 * @param string $key 缓存名称
+	 * @return boolean
+	 */
+	public function delete($key) {
+		return $this->mem->delete($key);
+	}
+
+
+	/**
+	 * 作废memcached的缓存，所有的项目
+	 * @return booean
+	 */
+	public function flush() {
+		return $this->mem->flush();
+	}
+}
