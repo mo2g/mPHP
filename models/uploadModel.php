e068b503aaf1fe30fa56da5935a75fe3568ba42c
Mo <moyancheng@gmail.com>
2016-08-10 16:22:19 +0800

    添加支持的上传文件类型 pdf、doc、docx、xls、xlsx、ppt、pptx

--- models/uploadModel.php
+++ models/uploadModel.php
@@ -55,6 +55,13 @@ class uploadModel {
 		'application/x-zip-compressed',
 		
 		'application/octet-stream',//二进制文件,待优化判断
+		'application/pdf',
+		'application/msword',// doc
+		'application/vnd.openxmlformats-officedocument.wordprocessingml.document',//docx
+		'application/vnd.ms-excel',//xls
+		'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',//xlsx
+		'application/vnd.ms-powerpoint',//ppt
+		'application/vnd.openxmlformats-officedocument.presentationml.presentation',//pptx
 	);//安全类型
 	public $safe_size;//安全上传文件大小
 	public $file;
